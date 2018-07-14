ac302129d08595ddb719f423c533acf70c8a4d99
kangze <kangze@DESKTOP-QADNNR1>
2018-07-14 20:02:48 +0800

    init project

---
+++ KingChat/Localization/znCN.lua
@@ -0,0 +1,7 @@
+if GetLocale() == "zhCN" then
+    if not KingChatLocalization then
+        KingChatLocalization = {}
+    end
+    KingChatLocalization["Close"] = "关闭"
+    KingChatLocalization["Save"] = "保存"
+end
